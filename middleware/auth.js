f7906b6ec17b1830e9a36427e77a1b45a124ab16
Ajay Patidar <ajaypatidar2112@gmail.com>
2024-04-10 03:46:27 +0530

    fixing User model error

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const user = require("../models/user");
+const user = require("../models/User");
 
 const verifyToken = (req,res,next)=>{
     const token = req.headers["authorization"];
